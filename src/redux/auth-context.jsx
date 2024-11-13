2460ff973bd13acd161671f4abd545f84120494f
Dharmraj07 <dharmrajh03@gmail.com>
2024-11-13 15:43:34 +0530

    first commit

---
+++ src/redux/auth-context.jsx
@@ -0,0 +1,35 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext({
+  token: "",
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [token, setToken] = useState(null);
+
+  const loginHandler = (token) => {
+    setToken(token);
+  };
+
+  const logoutHandler = () => {
+    setToken(null); // This will automatically make isLoggedIn false
+  };
+
+  const contextValue = {
+    token: token,
+    isLoggedIn: !!token, // Derived from token; no need to manually update
+    login: loginHandler,
+    logout: logoutHandler,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
